84c2748816fc491992a9a3db2abf82d12e5cca48
Hyunjun Ahn <hyunahn@c3r5s2.42gyeongsan.kr>
2025-05-25 20:10:36 +0900

    paser modified May25 20:10

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -4,6 +4,13 @@
 #include <string>
 #include <sstream>
 
+struct Command
+{
+  std::string verb;        // 명령어: "JOIN", "PRIVMSG" 등
+  std::string target;      // 대상: "#channel" 또는 "nickname"
+  std::string params;      // 파라미터(메시지 등)
+};
+
 class Parser
 {
   private:
@@ -11,6 +18,7 @@ class Parser
     Parser(/* args */);
     ~Parser();
     static bool is_command(std::string obj);
+    static Command parse_line(const std::string& line);
 };
 
 
